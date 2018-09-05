c63361978b53770d91fae98d72e413eeaa363c61
Arthur <gaartur4@gmail.com>
2018-09-05 22:06:32 +0300

    error boundary and person move to persons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -47,13 +48,13 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary key={person.id}><Person
               key={person.id}
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
               changed={(event) => this.nameChangeHandler(event, person.id)}
-            />
+            /></ErrorBoundary>
           })}
         </div>
       );
